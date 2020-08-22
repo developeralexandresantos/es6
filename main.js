4678b155ddce36196ef0bdfc5a6ab80f8a02fda7
Titio-Lele <gp-alexandresantos@uol.com.br>
2020-08-22 18:04:45 -0300

    desafio

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
-class minhaClass{
-    
-};
+class TodoList {
+    constructor() {
+        this.todos = [];
+    }
+
+    addTodo() {
+        this.todos.push('Novo todo');
+        console.log(this.todos);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.addTodo(); 
+}
+
+class Usuario {
+    constructor(email, senha) {
+        this.email = email;
+        this.senha = senha;
+        this.admin = false;
+    }
+
+    isAdmin() {
+        return this.admin;
+    }
+}
+
+class Admin extends Usuario{
+    constructor(email, senha) {
+        super();
+        this.admin = true;
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+
+console.log(User1.isAdmin());
+console.log(Adm1.isAdmin());
