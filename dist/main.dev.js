362b4340570774dc748cafb76b032373024891e5
Titio-Lele <gp-alexandresantos@uol.com.br>
2020-08-26 00:17:40 -0300

    Solved challenge 3.3

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -2,25 +2,21 @@
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -28,7 +24,9 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var TodoList = /*#__PURE__*/function () {
+var TodoList =
+/*#__PURE__*/
+function () {
   function TodoList() {
     _classCallCheck(this, TodoList);
 
@@ -52,7 +50,9 @@ document.getElementById("novotodo").onclick = function () {
   MinhaLista.addTodo();
 };
 
-var Usuario = /*#__PURE__*/function () {
+var Usuario =
+/*#__PURE__*/
+function () {
   function Usuario(email, senha) {
     _classCallCheck(this, Usuario);
 
@@ -71,17 +71,17 @@ var Usuario = /*#__PURE__*/function () {
   return Usuario;
 }();
 
-var Admin = /*#__PURE__*/function (_Usuario) {
+var Admin =
+/*#__PURE__*/
+function (_Usuario) {
   _inherits(Admin, _Usuario);
-
-  var _super = _createSuper(Admin);
 
   function Admin(email, senha) {
     var _this;
 
     _classCallCheck(this, Admin);
 
-    _this = _super.call(this);
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Admin).call(this));
     _this.email = email;
     _this.senha = senha;
     _this.admin = true;
@@ -120,7 +120,7 @@ var encontraGoogle = usuario.find(function (item) {
 });
 console.log(encontraGoogle);
 var multiplicador = usuario.map(function (item) {
-  var multiplicacao = _objectSpread(_objectSpread({}, item), {}, {
+  var multiplicacao = _objectSpread({}, item, {
     idade: item.idade * 2
   });
 
--- main.js
+++ main.js
@@ -4,14 +4,14 @@ class TodoList {
     }
 
     addTodo() {
-        this.todos.push('Novo todo');
+        this.todos.push("Novo todo");
         console.log(this.todos);
     }
 }
 
 const MinhaLista = new TodoList();
 
-document.getElementById('novotodo').onclick = function () {
+document.getElementById("novotodo").onclick = function () {
     MinhaLista.addTodo();
 };
 
@@ -25,7 +25,7 @@ class Usuario {
     isAdmin() {
         return `${this.admin}`;
     }
-};
+}
 
 class Admin extends Usuario {
     constructor(email, senha) {
@@ -34,38 +34,54 @@ class Admin extends Usuario {
         this.senha = senha;
         this.admin = true;
     }
-};
+}
 
-const User1 = new Usuario('email@teste.com', 'senha123');
-const Admin1 = new Admin('email@teste.com', 'senha123');
+const User1 = new Usuario("email@teste.com", "senha123");
+const Admin1 = new Admin("email@teste.com", "senha123");
 
 console.log(User1.isAdmin());
 
 const usuario = [
-    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
-    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
-    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-]
+    { nome: "Diego", idade: 23, empresa: "Rocketseat" },
+    { nome: "Gabriel", idade: 15, empresa: "Rocketseat" },
+    { nome: "Lucas", idade: 30, empresa: "Facebook" },
+];
 
-
-const idade = usuarios.map((item) => item.idade);
+const idade = usuario.map((item) => item.idade);
 console.log(idade);
 
-const trabalhaEMaiorQue18 = usuarios.filter((item) => item.idade > 18 && item.empresa === 'DreamMaker');
+const trabalhaEMaiorQue18 = usuario.filter(
+    (item) => item.idade > 18 && item.empresa === "DreamMaker"
+);
 console.log(trabalhaEMaiorQue18);
 
-const encontraGoogle = usuarios.find((item) => item === 'Google');
+const encontraGoogle = usuario.find((item) => item === "Google");
 console.log(encontraGoogle);
 
-const multiplicador = usuarios.map(function (item) {
-    const multiplicacao = ({ ...item, idade: item.idade * 2});
+const multiplicador = usuario.map(function (item) {
+    const multiplicacao = { ...item, idade: item.idade * 2 };
     return multiplicacao;
-})
+});
 
 const idadeMultiplicada = multiplicador.filter((item) => item.idade < 50);
 console.log(idadeMultiplicada);
 
-// 3.1
+// Desafio 3.1
 const arr = [1, 2, 3, 4, 5];
 
 console.log(arr.map((item) => item + 10));
+
+// Desafio 3.2
+const usuario1 = {nome: 'Diego', idade: 23 };
+const mostraIdade = ((usuario) => usuario.idade);
+mostraIdade(usuario1);
+
+// Desafio 3.3
+
+const nome = "Diego";
+const idade1 = 23;
+
+const mostraUsuario = ((nome = 'Diego', idade = 18) => ({nome, idade}));
+
+mostraUsuario(nome, idade1);
+mostraUsuario(nome);
